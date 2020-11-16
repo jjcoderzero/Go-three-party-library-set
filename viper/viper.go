92acedfcc84026f1da5c26bb1d36b3beba80e92d
Andrew <Andrew.X.Wan.-ND@disney.com>
2020-11-16 09:57:49 +0800

    add annotations

--- viper/viper.go
+++ viper/viper.go
@@ -1,8 +1,3 @@
-// Copyright © 2014 Steve Francia <spf@spf13.com>.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
 // Viper is an application configuration system.
 // It believes that applications can be configured a variety of ways
 // via flags, ENVIRONMENT variables, configuration files retrieved
